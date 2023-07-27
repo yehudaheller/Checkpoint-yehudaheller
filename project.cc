0af9e60e4d6d5b6fdc543612386297ea44d3907a
yehudaheller <116564740+yehudaheller@users.noreply.github.com>
2023-07-27 12:19:15 +0300

    allow user delete student,and starting calc top 10

--- project.cc
+++ project.cc
@@ -20,6 +20,7 @@ struct Student {
     int num_layers;
     int num_of_class;
     int grades[NUM_OF_GRADES];
+
 };
 
 struct School {
@@ -42,6 +43,10 @@ void processMenuOptions(struct Student *students, int *num_students, struct Scho
 void searchStudent(struct Student *students, int num_students) ;
 struct Student* searchStudentByName(struct Student *students, int num_students, const char *first_name, const char *last_name) ;
 void editStudentData(struct Student *students, int num_students) ;
+void deleteStudent(struct Student *students, int *num_students, struct School *school) ;
+
+
+
 
 //------------------------------main------------------------------------
 
@@ -120,7 +125,6 @@ void viewAllStudents(struct Student *students, int num_students) {
     }
 }
 
-// Function to handle user input and menu options
 void processMenuOptions(struct Student *students, int *num_students, struct School *school) {
     int choice;
     do {
@@ -141,18 +145,27 @@ void processMenuOptions(struct Student *students, int *num_students, struct Scho
             case 4: // Edit student data
                 editStudentData(students, *num_students);
                 break;
-            case 5: // Exit
+            case 5: // Delete a student
+                deleteStudent(students, num_students, school);
+                break;
+            case 6: // Find top 10 students with the specified grade in every level
+                int gradeNumber;
+                printf("Enter the grade number (1-%d): ", NUM_OF_GRADES);
+                scanf("%d", &gradeNumber);
+                if (gradeNumber >= 1 && gradeNumber <= NUM_OF_GRADES) {
+                    findTop10HighGrade(school, gradeNumber);
+                } else {
+                    printf("Invalid grade number. Please try again.\n");
+                }
+                break;
+            case 7: // Exit
                 printf("Exiting...\n");
                 break;
             default:
                 printf("Invalid choice. Try again.\n");
         }
-
-        // ... Existing code ...
-
-    } while (choice != 5);
+    } while (choice != 7);
 }
-
 
 
 
@@ -227,15 +240,17 @@ void addStudentToDB(struct School *school, struct Student *student) {
     school->DB[student->num_layers - 1][student->num_of_class - 1] = student;
 }
 
-// Function to display the menu options
 void displayMenu() {
     puts("MENU:");
     puts("1. Add new student");
     puts("2. View all students");
     puts("3. Search for a student");
-    puts("4. Edit student data"); // New option for editing student data
-    puts("5. Exit");
+    puts("4. Edit student data");
+    puts("5. Delete a student");
+    puts("6. Find top 10 students with the specified grade in every level");
+    puts("7. Exit");
 }
+
 
 
 // Function to save student data to the file
@@ -328,6 +343,55 @@ void editStudentData(struct Student *students, int num_students) {
         printf("Student not found.\n");
     }
 }
+
+//delete existing student
+void deleteStudent(struct Student *students, int *num_students, struct School *school) {
+    char first_name[LEN];
+    char last_name[LEN];
+
+    printf("Enter student's first name: ");
+    scanf("%s", first_name);
+    printf("Enter student's last name: ");
+    scanf("%s", last_name);
+
+    // Find the student in the array by name
+    struct Student *foundStudent = searchStudentByName(students, *num_students, first_name, last_name);
+    if (foundStudent != NULL) {
+        // Remove the student from the school database
+        school->DB[foundStudent->num_layers - 1][foundStudent->num_of_class - 1] = NULL;
+
+        // Find the index of the student in the array
+        int index = -1;
+        for (int i = 0; i < *num_students; i++) {
+            if (strcmp(students[i].first_name, first_name) == 0 &&
+                strcmp(students[i].last_name, last_name) == 0) {
+                index = i;
+                break;
+            }
+        }
+
+        // Shift the elements in the array to overwrite the deleted student
+        if (index >= 0) {
+            for (int i = index; i < *num_students - 1; i++) {
+                students[i] = students[i + 1];
+            }
+            (*num_students)--;
+
+            // Save changes to the file after deletion
+            if (!saveStudentData("db_file.txt", students, *num_students)) {
+                printf("Error saving changes to the file.\n");
+                exit(EXIT_FAILURE);
+            }
+
+            printf("Student '%s %s' has been deleted.\n", first_name, last_name);
+        } else {
+            printf("Student not found.\n");
+        }
+    } else {
+        printf("Student not found.\n");
+    }
+}
+
 
 
 
